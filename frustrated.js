a7cefcc100878f029d65b7ec00c7f35e2932bc30
Isis <isiswenger@gmail.com>
2013-11-06 21:31:22 -0800

    Update frustrated.js

--- frustrated.js
+++ frustrated.js
@@ -3,13 +3,14 @@ function clickCounter() {
 	document.onclick = function(event) {
 
 		document.eventsArr.push(new Date())
-		console.log(document.eventsArr);
-		for (i= document.eventsArr.length-5; i<=document.eventsArr.length-2; i++){
-			if((document.eventsArr[i+1]-document.eventsArr[i])/3+(document.eventsArr[i+2]-document.eventsArr[i+1])/3+(document.eventsArr[i+3]-document.eventsArr[i+2])/3 <= 800){
-				document.eventsArr = [];
-				document.getElementById("body").dispatchEvent(customerFrustratedEvent);
+		if (document.eventsArr.length > 5){
+			for (i= document.eventsArr.length-5; i<=document.eventsArr.length-2; i++){
+				if((document.eventsArr[i+1]-document.eventsArr[i])/3+(document.eventsArr[i+2]-document.eventsArr[i+1])/3+(document.eventsArr[i+3]-document.eventsArr[i+2])/3 <= 800){
+					document.eventsArr = [];
+					document.getElementById("body").dispatchEvent(customerFrustratedEvent);
+				}
 			}
-		}
+		}	
 	}
 };
 
@@ -35,5 +36,4 @@ var customerFrustratedEvent = new CustomEvent(
 document.addEventListener("customerFrustrated", function(e){ 
 /*	alert("Meow=^.^="); */
 }, false);
-
 
