572c107f3e5ddbd01bf8aee7b63eff2a2ffc8502
Avlok Kohli <avlok.kohli@gmail.com>
2013-11-13 16:16:09 -0800

    fixing bug with frustrated.js. adding example.html

--- frustrated.js
+++ frustrated.js
@@ -1,41 +1,37 @@
 /* Frustrated.JS */
 function clickCounter() {
-	document.onclick = function(event) {
+  document.onclick = function(event) {
 
-		document.eventsArr.push(new Date())
-		if (document.eventsArr.length > 5){
-			for (i= document.eventsArr.length-5; i<=document.eventsArr.length-2; i++){
-				if((document.eventsArr[i+1]-document.eventsArr[i])/3+(document.eventsArr[i+2]-document.eventsArr[i+1])/3+(document.eventsArr[i+3]-document.eventsArr[i+2])/3 <= 800){
-
-
-					var customerFrustratedEvent = new CustomEvent(
-						"customerFrustrated", 
-						{
-							detail: {
-								message: "Customer is frustrated!",
-								time: new Date(),
-							},
-							bubbles: true,
-							cancelable: true
-						}
-					);
+    document.eventsArr.push(new Date())
+    if (document.eventsArr.length > 5){
+      for (i= document.eventsArr.length-5; i<=document.eventsArr.length-2; i++){
+        if((document.eventsArr[i+1]-document.eventsArr[i])/3+(document.eventsArr[i+2]-document.eventsArr[i+1])/3+(document.eventsArr[i+3]-document.eventsArr[i+2])/3 <= 800){
 
 
-					document.eventsArr = [];
-					document.getElementById("body").dispatchEvent(customerFrustratedEvent);
-				}
-			}
-		}	
-	}
+          var customerFrustratedEvent = new CustomEvent(
+            "customerFrustrated", 
+            {
+              detail: {
+                message: "Customer is frustrated!",
+                time: new Date(),
+              },
+              bubbles: true,
+              cancelable: true
+            }
+          );
+
+
+          document.eventsArr = [];
+          document.getElementsByTagName("body")[0].dispatchEvent(customerFrustratedEvent);
+        }
+      }
+    } 
+  }
 };
 
 
 window.onload = function() {
-	document.eventsArr = [];
-	clickCounter();
+  document.eventsArr = [];
+  clickCounter();
 }
 
-
-document.addEventListener("customerFrustrated", function(e){ 
-/*	alert("Meow=^.^=");  */
-}, false);
