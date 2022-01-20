69c1def44ee7ec22ce48c910c8646b2024b25a74
Montana Mendy <montana@linux.com>
2022-01-19 16:54:23 -0800

    Refactored, a bit cleaner. Further range for annotations.

--- frustrated.js
+++ frustrated.js
@@ -1,32 +1,30 @@
-/* Frustrated.JS */
-(function () {
-  var eventsArr = [];
+(function() {
+    var eventsArr = [];
 
-  document.addEventListener("click", function(even) {
-    eventsArr.push(new Date());
+    document.addEventListener("click", function(even) {
+        eventsArr.push(new Date());
 
-    if (eventsArr.length > 5){
-      for (var i= eventsArr.length-5; i<=eventsArr.length-2; i++){
-        if ((eventsArr[i+1]-eventsArr[i])/3+(eventsArr[i+2]-eventsArr[i+1])/3+(eventsArr[i+3]-eventsArr[i+2])/3 <= 800){
+        if (eventsArr.length > 5) {
+            for (var i = eventsArr.length - 5; i <= eventsArr.length - 2; i++) {
+                if ((eventsArr[i + 1] - eventsArr[i]) / 3 + (eventsArr[i + 2] - eventsArr[i + 1]) / 3 + (eventsArr[i + 3] - eventsArr[i + 2]) / 3 <= 800) {
 
 
-          var customerFrustratedEvent = new CustomEvent(
-            "customerFrustrated", 
-            {
-              detail: {
-                message: "Customer is frustrated!",
-                time: new Date(),
-              },
-              bubbles: true,
-              cancelable: true
+                    var customerFrustratedEvent = new CustomEvent(
+                        "customerFrustrated", {
+                            detail: {
+                                message: "Customer is frustrated!",
+                                time: new Date(),
+                            },
+                            bubbles: true,
+                            cancelable: true
+                        }
+                    );
+
+
+                    eventsArr = [];
+                    document.dispatchEvent(customerFrustratedEvent);
+                }
             }
-          );
-
-
-          eventsArr = [];
-          document.dispatchEvent(customerFrustratedEvent);
         }
-      }
-    } 
-  });
+    });
 })();
